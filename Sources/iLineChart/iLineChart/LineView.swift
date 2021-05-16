205eb100ce2277e01c6cf5ed27d33887e9b07e54
Ben Hakes <bhhakes@gmail.com>
2021-05-16 14:29:04 -0500

    add hightest and lowest points

--- Sources/iLineChart/iLineChart/LineView.swift
+++ Sources/iLineChart/iLineChart/LineView.swift
@@ -21,9 +21,12 @@ struct LineView: View {
     @State private var dragLocation:CGPoint = .zero
     @State private var indicatorLocation:CGPoint = .zero
     @State private var closestPoint: CGPoint = .zero
+    @State private var minimumPoint: CGPoint?
+    @State private var maximumPoint: CGPoint?
     @State private var opacity:Double = 0
     @State private var currentDataNumber: Double = 0
     @State private var hideHorizontalLines: Bool = false
+    @State private var showHighAndLowValues: Bool = false
     private var defaultHorizontalLines: Bool = false
     
     public init(data: [Double],
@@ -31,7 +34,8 @@ struct LineView: View {
                 legend: String? = nil,
                 style: ChartStyle = Styles.lineChartStyleOne,
                 numberFormat: String? = "%.1f",
-                hideHorizontalLines: Bool = false) {
+                hideHorizontalLines: Bool = false,
+                showHighAndLowValues: Bool = false) {
         self.defaultHorizontalLines = hideHorizontalLines
         self._hideHorizontalLines = State(initialValue: hideHorizontalLines)
         self.data = ChartData(points: data)
@@ -40,6 +44,7 @@ struct LineView: View {
         self.style = style
         self.numberFormat = numberFormat!
         self.darkModeStyle = style.darkModeStyle != nil ? style.darkModeStyle! : Styles.lineViewDarkMode
+        self.showHighAndLowValues = showHighAndLowValues
         
     }
     
@@ -58,16 +63,33 @@ struct LineView: View {
                             .foregroundColor(self.colorScheme == .dark ? self.darkModeStyle.legendTextColor : self.style.legendTextColor)
                     }
                 }.offset(x: 0, y: 20)
+                
                 ZStack{
-                    GeometryReader{ reader in
+                    GeometryReader { reader in
                         Rectangle()
                             .foregroundColor(self.colorScheme == .dark ? self.darkModeStyle.backgroundColor : self.style.backgroundColor)
                         if(self.showLegend){
                             Legend(data: self.data,
-                                   frame: .constant(reader.frame(in: .local)), hideHorizontalLines: self.$hideHorizontalLines)
+                                   frame: .constant(reader.frame(in: .local)),
+                                   hideHorizontalLines: self.$hideHorizontalLines)
                                 .transition(.opacity)
                                 .animation(Animation.easeOut(duration: 1).delay(1))
                         }
+                        
+                        let minimumPoint = self.getMinimumDataPoint(width: geometry.frame(in: .local).size.width-30,
+                                                                     height: 240)
+                        let maximumPoint = self.getMaximumDataPoint(width: geometry.frame(in: .local).size.width-30,
+                                                                     height: 240)
+                        Text("2,233")
+                            .offset(x: minimumPoint.x,
+                                    y: minimumPoint.y)
+                            .foregroundColor(self.style.textColor)
+                        
+                        Text("3,322")
+                            .offset(x: maximumPoint.x,
+                                    y: maximumPoint.y + 180)
+                            .foregroundColor(self.style.textColor)
+                        
                         Line(data: self.data,
                              frame: .constant(CGRect(x: 0, y: 0, width: reader.frame(in: .local).width - 30, height: reader.frame(in: .local).height)),
                              touchLocation: self.$indicatorLocation,
@@ -87,30 +109,35 @@ struct LineView: View {
                     }
                     .frame(width: geometry.frame(in: .local).size.width, height: 240)
                     .offset(x: 0, y: 40 )
-                    MagnifierRect(currentNumber: self.$currentDataNumber, valueSpecifier: self.numberFormat)
+                    MagnifierRect(currentNumber: self.$currentDataNumber,
+                                  valueSpecifier: self.numberFormat)
                         .opacity(self.opacity)
                         .offset(x: self.dragLocation.x - geometry.frame(in: .local).size.width/2, y: 36)
                 }
                 .frame(width: geometry.frame(in: .local).size.width, height: 240)
                 .gesture(DragGesture()
-                .onChanged({ value in
-                    self.dragLocation = value.location
-                    self.indicatorLocation = CGPoint(x: max(value.location.x-30,0), y: 32)
-                    self.opacity = 1.0
-                    self.closestPoint = self.getClosestDataPoint(toPoint: value.location, width: geometry.frame(in: .local).size.width-30, height: 240)
-                    self.hideHorizontalLines = true
-                })
-                    .onEnded({ value in
-                        self.opacity = 0
-                        self.hideHorizontalLines = false
-                        print(self.defaultHorizontalLines)
-                    })
+                            .onChanged({ value in
+                                self.dragLocation = value.location
+                                self.indicatorLocation = CGPoint(x: max(value.location.x-30,0), y: 32)
+                                self.opacity = 1.0
+                                self.closestPoint = self.getClosestDataPoint(toPoint: value.location,
+                                                                             width: geometry.frame(in: .local).size.width-30,
+                                                                             height: 240)
+                                self.hideHorizontalLines = true
+                            })
+                            .onEnded({ value in
+                                self.opacity = 0
+                                self.hideHorizontalLines = false
+                                print(self.defaultHorizontalLines)
+                            })
                 )
             }
         }
     }
     
-    func getClosestDataPoint(toPoint: CGPoint, width:CGFloat, height: CGFloat) -> CGPoint {
+    func getClosestDataPoint(toPoint: CGPoint,
+                             width:CGFloat,
+                             height: CGFloat) -> CGPoint {
         let points = self.data.onlyPoints()
         let stepWidth: CGFloat = width / CGFloat(points.count-1)
         let stepHeight: CGFloat = height / CGFloat(points.max()! + points.min()!)
@@ -122,14 +149,52 @@ struct LineView: View {
         }
         return .zero
     }
+    
+    func getMinimumDataPoint(width:CGFloat,
+                             height: CGFloat) -> CGPoint {
+        let points = self.data.onlyPoints()
+        let pointsEnumerated = points.enumerated()
+        guard let indexOfMinPoint = pointsEnumerated.min(by: { $0.element < $1.element })?.offset else {
+            return .zero
+        }
+        
+        let stepWidth: CGFloat = width / CGFloat(points.count-1)
+        let stepHeight: CGFloat = height / CGFloat(points.max()! + points.min()!)
+        
+        if (indexOfMinPoint >= 0 && indexOfMinPoint < points.count){
+            self.currentDataNumber = points[indexOfMinPoint]
+            return CGPoint(x: CGFloat(indexOfMinPoint)*stepWidth,
+                           y: CGFloat(points[indexOfMinPoint])*stepHeight)
+        }
+        return .zero
+    }
+    
+    func getMaximumDataPoint(width:CGFloat,
+                             height: CGFloat) -> CGPoint {
+        let points = self.data.onlyPoints()
+        let pointsEnumerated = points.enumerated()
+        guard let indexOfMaxPoint = pointsEnumerated.max(by: { $0.element > $1.element })?.offset else {
+            return .zero
+        }
+        
+        let stepWidth: CGFloat = width / CGFloat(points.count-1)
+        let stepHeight: CGFloat = height / CGFloat(points.max()! + points.min()!)
+        
+        if (indexOfMaxPoint >= 0 && indexOfMaxPoint < points.count){
+            self.currentDataNumber = points[indexOfMaxPoint]
+            return CGPoint(x: CGFloat(indexOfMaxPoint)*stepWidth,
+                           y: CGFloat(points[indexOfMaxPoint])*stepHeight)
+        }
+        return .zero
+    }
 }
 
 struct LineView_Previews: PreviewProvider {
     static var previews: some View {
         Group {
-            LineView(data: [8,23,54,32,12,37,7,23,43], title: "Full chart", style: Styles.lineChartStyleOne)
             
-            LineView(data: [282.502, 284.495, 283.51, 285.019, 285.197, 286.118, 288.737, 288.455, 289.391, 287.691, 285.878, 286.46, 286.252, 284.652, 284.129, 284.188], title: "Full chart", style: Styles.lineChartStyleOne)
+            LineView(data: [282.502, 284.495, 283.51, 285.019, 285.197, 286.118, 288.737, 288.455, 289.391, 287.691, 285.878, 286.46, 286.252, 284.652, 284.129, 284.188],
+                     style: Styles.lineChartStyleOne)
             
         }
     }
